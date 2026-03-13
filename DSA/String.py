ff041236e58c23c37418cec3105f5e033ade7f15
TANUJA BAIRWA <141355091+msbatb@users.noreply.github.com>
2026-03-13 15:45:53 +0530

    Refactor String.py to include new algorithm comments
    Commented out the Roman to Integer implementation and added a new section for the Longest Substring without repeating characters algorithm.

--- DSA/String.py
+++ DSA/String.py
@@ -1,10 +1,24 @@
-s="III"
-d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-total = 0
-for i in range(len(s)):
-    if i < len(s) - 1 and d[s[i]] < d[s[i + 1]]:
-        total -= d[s[i]]
-    else:
-        total += d[s[i]]
+#Roman to Integer
+# s="III"
+# d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+# total = 0
+# for i in range(len(s)):
+#     if i < len(s) - 1 and d[s[i]] < d[s[i + 1]]:
+#         total -= d[s[i]]
+#     else:
+#         total += d[s[i]]
+# print(total)
 
-print(total)
+#Logest Substring without repeating characters 
+# s = "abcabcbb"
+# ch_set=set()
+# left=0
+# max_len=0
+
+# for right in range(len(s)):
+#     while s[right] in ch_set:
+#         ch_set.remove(s[left])
+#         left+=1
+#     ch_set.add(s[right])
+#     max_len=max(max_len,right-left+1)
+# print(max_len)
